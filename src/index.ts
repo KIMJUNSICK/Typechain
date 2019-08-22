298289074fb2450dc53096d20aa3d443e48d72e5
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-22 12:50:53 +0900

    basic Setting for Blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + data + timestamp).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -21,8 +32,12 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "a3s2d4as32d4a3", "", "Hello", 123465);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
