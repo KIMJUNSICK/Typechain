3f4f280adedf6472ac7b1053c192c72fe13a41c9
Kim Jun Sik <jinaganda5@gmail.com>
2019-08-22 11:24:14 +0900

    Interface on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-const name = "junsik",
-  age = 25,
-  gender = "male";
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "junsik",
+  age: 25,
+  gender: "male"
+};
 
 export {};
 
-const sayHello = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age} & gender is ${gender}`;
+const sayHello = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age} & gender is ${
+    person.gender
+  }!`;
 };
 
-console.log(sayHello(name, age, gender));
+console.log(sayHello(person));
